b78b53e4c6efbcd688e78a16071fd76d6cbb0033
mtouama <mina.touama@etu.unistra.fr>
2025-01-22 21:57:15 +0100

    update clean code

--- FRONTEND/src/main.ts
+++ FRONTEND/src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
 import { appRoutingProviders } from './app/app.routes'; 
 
 bootstrapApplication(AppComponent, {
